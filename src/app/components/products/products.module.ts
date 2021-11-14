39b2a2512a756e834672ec1322b00484a083bd1a
Lora <cruzeiro@abv.bg>
2021-11-14 23:20:09 +0200

    added top navigation and meat page

--- src/app/components/products/products.module.ts
+++ src/app/components/products/products.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {AllProductsComponent} from './all-products/all-products.component';
 import {RouterModule} from '@angular/router';
 import {SharedModule} from '../shared/shared.module';
+import { MeatComponent } from './meat/meat.component';
 
 
 @NgModule({
   declarations: [
     AllProductsComponent,
+    MeatComponent,
   ],
   imports: [
     CommonModule,
@@ -15,6 +17,7 @@ import {SharedModule} from '../shared/shared.module';
     RouterModule.forChild([
       {path: '', pathMatch: 'full', redirectTo: 'home'},
       {path: 'all', component: AllProductsComponent},
+      {path: 'meat', component: MeatComponent},
     ]),
   ]
 })
--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {AsideNavigationComponent} from './aside-navigation/aside-navigation.component';
 import {BottomNavigationComponent} from './bottom-navigation/bottom-navigation.component';
 import {RouterModule} from '@angular/router';
+import {TopNavigationComponent} from './top-navigation/top-navigation.component';
 
 @NgModule({
   declarations: [
     AsideNavigationComponent,
-    BottomNavigationComponent
+    BottomNavigationComponent,
+    TopNavigationComponent
   ],
   imports: [
     CommonModule,
@@ -15,7 +17,9 @@ import {RouterModule} from '@angular/router';
   ],
   exports: [
     AsideNavigationComponent,
-    BottomNavigationComponent
+    BottomNavigationComponent,
+    TopNavigationComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
